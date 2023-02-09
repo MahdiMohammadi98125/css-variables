36521224f3d7dd727b15520133d91f5814ec12bb
MahdiMohammadi98125 <109587937+MahdiMohammadi98125@users.noreply.github.com>
2023-02-09 11:54:35 -0800

    the functionality of the inputs added

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 "use strict";
+// takes all the input
+const inputs = document.querySelectorAll(".controls input");
+console.log(inputs);
+
+// change the value of the inputs value
+const changeAmount = function () {
+  const suffix = this.dataset.sizing || "";
+  document.documentElement.style.setProperty(
+    `--${this.name}`,
+    this.value + suffix
+  );
+};
+// add eventlistener for each input
+inputs.forEach((input) => input.addEventListener("change", changeAmount));
+inputs.forEach((input) => input.addEventListener("mousemove", changeAmount));
